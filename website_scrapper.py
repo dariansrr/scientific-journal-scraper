b502521716d678dbd7eb979f2d33b155c55549af
Darian Rabbani <dariansrr@gmail.com>
2022-02-02 21:06:58 -0300

    started scrap_article function

--- website_scrapper.py
+++ website_scrapper.py
@@ -1,15 +1,16 @@
 from bs4 import BeautifulSoup
+import re
 import requests
 
 def scrap_all_issues(url):
-    print('starting to scrap', url)
+    print("starting to scrap", url)
     main_page = requests.get(url)
 
     if(main_page.status_code == 200):
-        print('connection to main site: OK')
+        print("connection to main site: OK")
 
         # Parse main page
-        main_soup = BeautifulSoup(main_page.content, 'lxml')
+        main_soup = BeautifulSoup(main_page.content, "lxml")
 
         # Get all issues
         list_of_issues = main_soup.find("ul", {"class": "issues_archive"}).find_all("a", {"class": "title"})
@@ -25,7 +26,7 @@ def scrap_all_issues(url):
 def scrap_issue(issues_list):
 
     ######### ONLY FOR TEST #######
-    issues_list = issues_list[:2]
+    issues_list = issues_list[:1]
     ###############################
 
 
@@ -33,17 +34,17 @@ def scrap_issue(issues_list):
 
         print("-------- STARTINNG NEW ISSUE --------")
         # Get issue url    
-        issue_url = issue['href']
+        issue_url = issue["href"]
         
         # Get issue page
         issue_page = requests.get(issue_url)
         
         if(issue_page.status_code == 200):
             # Parse issue page
-            issue_soup = BeautifulSoup(issue_page.content, 'lxml')
+            issue_soup = BeautifulSoup(issue_page.content, "lxml")
 
             # Remove <span> tags for cleaned result in the h3 lists
-            for s in issue_soup.select('span'):
+            for s in issue_soup.select("span"):
                 s.extract()
             
             # Get all articles
@@ -54,7 +55,41 @@ def scrap_issue(issues_list):
             for h3 in list_of_articles:
                 clean_list_of_articles.append(h3.find("a"))
 
-            print(clean_list_of_articles)
+            scrap_article(clean_list_of_articles)
 
         else:
             print("Wasn't able to access the issue page " + str(issue_url) )
+
+def scrap_article(article_list):
+    
+    ######### ONLY FOR TEST #######
+    article_list = article_list[:2]
+    ###############################
+    
+    for article in article_list:
+        # Get issue url
+        article_url = article["href"]
+
+        # Get issue page
+        article_page = requests.get(article_url)
+        if(article_page.status_code == 200):
+            # Parse article page
+            article_soup = BeautifulSoup(article_page.content, "lxml")
+
+            # Create header for final list
+            # article_data = ['Ano', 'Revista', 'Titulo', 'Autores', 'Resumo', 'Palavras-chave']
+
+            # Creates empty list with defined size, using '' as default for empty
+            article_data = [""] * 6
+
+            # Get year
+            article_data[0] = article_soup.find("div", {"class": "csl-entry"}).text
+            article_data[0] = re.findall("\(([0-9]{4})", article_data[0])[0]
+                        
+            # Get title
+            article_data[2] = article_soup.find("h1", {"class": "page_title"}).text.strip()
+            
+            print(article_data)
+
+        else:
+            print("Wasn't able to access the article page " + str(article_url) )
